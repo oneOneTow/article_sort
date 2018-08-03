09406f1c98f0be8144d26bbafbbd858c94008c9b
LuZhiqing <zhiqing.lu@yinyuetai.com>
2018-08-03 18:46:31 +0800

    完善投票方法

--- src/main/java/com/redis/demo/controllers/ArticleControllerImpl.java
+++ src/main/java/com/redis/demo/controllers/ArticleControllerImpl.java
@@ -3,7 +3,6 @@ package com.redis.demo.controllers;
 import com.redis.demo.service.ArticleService;
 import com.redis.demo.vo.Article;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
